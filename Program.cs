d8ab1f375307ed9ff0f607ce97dbe1b5c99dcd07
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-03-15 09:39:24 +0200

    Configure RazorPages

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();//add mvc service
+
+builder.Services.AddRazorPages(); //add razor pages service
 
 var app = builder.Build();
 
-app.UseStaticFiles();
+app.UseStaticFiles();  //serving static content from wwwroot
 
+app.MapRazorPages();// map razor pages
 
-app.MapDefaultControllerRoute();
+app.MapDefaultControllerRoute(); //maping the home page
 
 app.Run();
