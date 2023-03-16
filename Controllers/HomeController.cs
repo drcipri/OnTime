debd6dcdce51d63765ac7bcc9972aeb13cd9df89
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-03-16 12:17:57 +0200

    Home Controller update
    -Include related objects(Classification)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,21 @@
-﻿namespace OnTime.Controllers
+﻿using Microsoft.EntityFrameworkCore;
+using OnTime.Models;
+using OnTime.Models.Repository;
+
+namespace OnTime.Controllers
 {
     public class HomeController: Controller
     {
+        private readonly IRepositoryAppointment _repository;
+        public HomeController(IRepositoryAppointment repository)
+        {
+            _repository = repository;
+        }
         public ViewResult Index()
         {
-            return View();
+            return View(_repository.Appointments.Where(c => c.ClassificationId == 1 && c.Classification.Name == ClassificationTypes.Awaiting)
+                                                .Include(c => c.Classification)
+                                                .OrderBy(c => c.Id).ToList());
         }
     }
 }
